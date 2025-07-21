af07123166d52dbc572f5eba178f89b8bfec61de
Pratyush Shukla <ps4534@nyu.edu>
2025-07-21 17:32:57 +0530

    fix torch2onnx script

--- recognition/arcface_torch/torch2onnx.py
+++ recognition/arcface_torch/torch2onnx.py
@@ -6,12 +6,13 @@ import torch
 def convert_onnx(net, path_module, output, opset=11, simplify=False):
     assert isinstance(net, torch.nn.Module)
     img = np.random.randint(0, 255, size=(112, 112, 3), dtype=np.int32)
-    img = img.astype(np.float)
+    img = img.astype(np.float32)
     img = (img / 255. - 0.5) / 0.5  # torch style norm
     img = img.transpose((2, 0, 1))
     img = torch.from_numpy(img).unsqueeze(0).float()
 
-    weight = torch.load(path_module)
+    device = "cuda" if torch.cuda.is_available() else "cpu"
+    weight = torch.load(path_module, map_location=torch.device(device))
     net.load_state_dict(weight, strict=True)
     net.eval()
     torch.onnx.export(net, img, output, input_names=["data"], keep_initializers_as_inputs=False, verbose=False, opset_version=opset)
@@ -31,7 +32,7 @@ if __name__ == '__main__':
     from backbones import get_model
 
     parser = argparse.ArgumentParser(description='ArcFace PyTorch to onnx')
-    parser.add_argument('input', type=str, help='input backbone.pth file or path')
+    parser.add_argument('--input', type=str, help='input backbone.pth file or path')
     parser.add_argument('--output', type=str, default=None, help='output onnx path')
     parser.add_argument('--network', type=str, default=None, help='backbone network')
     parser.add_argument('--simplify', type=bool, default=False, help='onnx simplify')
